9b1b43c096e3ef5d59231efb36a72b48330d9299
Your Name <youremail@yourdomain.com>
2023-03-29 14:08:31 -0700

    Toggle-able buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,4 +7,27 @@ function alertUser(){
 }
 //3. add the function as event listener
 // used if  we do not have an option parameter  button.addEventListener("click", alertUser);
-button.addEventListener("click", alertUser, {once: true});
+//button.addEventListener("click", alertUser, {once: true});
+
+//button.addEventListener("click", alertUser, {once:true});
+
+function changeBGPink()
+{
+    document.body.classList.add('pinkBG')
+    //document.body.classList.toggle('pinkBG')
+}
+button.addEventListener("click", changeBGPink);
+
+function changeText()
+{
+    //if it says click me! change it to click
+    //else chang it to clicked
+    if (button.textContent=== "Click Me!"){
+        button.textContent= 'Clicked!'
+    }
+    else {
+        button.textContent= "Click Me!"
+    }
+
+}
+button.addEventListener("click", changeText);
