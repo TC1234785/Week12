7d999063bec52ac0fb733a8985c925d69227e2af
Your Name <youremail@yourdomain.com>
2023-04-05 12:30:38 -0700

    Change text color, add elements

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,8 +18,8 @@ function changeBGPink()
 }
 button.addEventListener("click", changeBGPink);
 
-function changeText()
-{
+function changeText(event)
+{ console.log(event)
     //if it says click me! change it to click
     //else chang it to clicked
     if (button.textContent=== "Click Me!"){
@@ -31,3 +31,51 @@ function changeText()
 
 }
 button.addEventListener("click", changeText);
+
+//1. find the div
+const buttonContainer = document.querySelector(".button-container")
+//2. define changeBGGreen function
+function changeBGGreen(event){
+    if(event.target.tagName === "BUTTON"){
+        event.target.classList.add('greenBG')
+    }
+}
+///3. add the fnc as event listener
+buttonContainer.addEventListener("mouseover", changeBGGreen)
+
+//Define a function to change text color
+function changeTextColor(event)
+{
+    //with style
+//event.target.style.color = event.target.textContent
+
+    //with classList
+//event.target.classList.add(event.target.textContent)
+if(event.target.tagName === "BUTTON"){
+    event.target.classList.add(event.target.textContent)
+}
+
+//if 'red button is clicked -> the text color should be red
+}
+buttonContainer.addEventListener("click", changeTextColor)
+
+//1. find the element
+//2. define function
+function addNewElements(){
+    //createElement x2 -> button and p
+    const newButton = document.createElement('button');
+    const newP = document.createElement('p');
+
+    newButton.addEventListener("mouseover", changeBGGreen)
+    newP.addEventListener("mouseover", changeBGGreen)
+    //append Child x2
+    document.body.appendChild(newP)
+    document.body.appendChild(newButton)    
+    
+    //textContent x2
+    newButton.textContent = "purple"
+    newP.textContent = "something"
+    
+}
+//3. addEventListener
+button.addEventListener("click", addNewElements)
